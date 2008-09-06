ab7f487d6ed82108b4a1f80c68c424e1825db510
Mathias Sulser <suls@suls.org>
2008-09-07 01:58:17 +0200

    league code spec implemented

--- spec/suhv_service_spec.rb
+++ spec/suhv_service_spec.rb
@@ -16,6 +16,11 @@ describe "SUHV service plugin" do
 end
 
 describe "SUHV league codes" do
-  it "should be defined in a constant LEAGUE_CODES" 
-  it "should return league code 111 for 'Swiss Mobiliar League Herren'"
+  it "should be defined in a constant LEAGUE_CODES" do
+    SUHV::LEAGUE_CODES.should be_a_kind_of(Hash)
+  end
+  
+  it "should return league code 111 for 'Swiss Mobiliar League Herren'" do
+    SUHV::LEAGUE_CODES[111].should eql('Swiss Mobiliar League Herren')
+  end
 end
