dc438a217628d086890c238e5b20589bae8eeea8
Mathias Sulser <suls@suls.org>
2008-09-07 00:48:24 +0200

    cleaned up spec and added spec for league codes

--- spec/suhv_service_spec.rb
+++ spec/suhv_service_spec.rb
@@ -5,57 +5,17 @@ require "suhv_service"
 describe "SUHV service plugin" do
   
   before(:each) do
-    
+    @suhv = SUHV::League.new
   end
   
   it "should be able to query the echo function" do
-    
-    suhv = SUHV::League.new
     output = input = "test " * 3
-    suhv.echo(input).should eql(output)
+    @suhv.echo(input).should eql(output)
   end
+  
+end
 
-  after(:each) do
-    
-  end
-  
-  # describe "generated stubs" do
-  #    
-  #    before(:each) do
-  #      @rake["suhv_service:generate"]
-  #      
-  #      require 'rubygems'
-  #      gem 'soap4r'
-  #      require 'suhv_serviceDriver'
-  #    end
-  #    
-  #    it "should be able to query the echo function" do
-  #      suhv = SUHV::League.new
-  #      output = input = "test " * 3
-  #      suhv.echo(input).should eql(output)
-  #    end
-  #    
-  #  end
-  #  
-   # describe "installation" do
-   #   
-   #   require "rake"
-   #     
-   #   # it "should call the suhv_service:generate task" do
-   #   # end
-   #   before(:each) do
-   #     setup_rake_task_and_initialsie_plugin
-   #   end
-   #   
-   #   it "should generate stubs by calling suhv_service:generate task" do
-   #     gen_dir = Dir.entries File.dirname(__FILE__) + "/../gen/"
-   #     gen_dir.size.should be(0)
-   #     
-   #     @rake["suhv_service:generate"].invoke
-   #     
-   #     gen_dir.size.should be(3)
-   #   end
-   #   
-   # end
-  
+describe "SUHV league codes" do
+  it "should be defined in a constant LEAGUE_CODES" 
+  it "should return league code 111 for 'Swiss Mobiliar League Herren'"
 end
